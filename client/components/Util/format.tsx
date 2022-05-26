ceaf64cd91c0fe7ed0ca64f1f7952bff9c9b401a
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2022-05-26 07:12:59 +0100

    remove index

--- client/components/Util/format.tsx
+++ client/components/Util/format.tsx
@@ -3,7 +3,6 @@ export const formatTime = (t: Date) => {
     return d.toLocaleTimeString()
   }
 
-
  export  function formatHash(str:string,index:number,chr:string) {
     if(index > str.length-1) return str;
     return str.substring(0,index).slice(0 ,6) + chr + str.substring(index+1).slice(-6); 
